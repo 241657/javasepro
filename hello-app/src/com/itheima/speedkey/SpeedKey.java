f5f31f876601a14e2885af8642b061c0357ae707
cx-cx-cx <2416571038@qq.com>
2022-10-12 21:44:53 +0800

    first commit

---
+++ hello-app/src/com/itheima/speedkey/SpeedKey.java
@@ -0,0 +1,9 @@
+package com.itheima.speedkey;
+
+public class SpeedKey {
+    public static void main(String[] args) {
+        System.out.println("Hello World2");
+        System.out.println("Hello World2");
+        System.out.println("Hello World2");
+    }
+}
