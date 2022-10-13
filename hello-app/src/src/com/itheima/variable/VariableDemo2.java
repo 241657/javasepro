7834addf3ecd672bb21727a2763ca963f6909582
241657 <87233108+241657@users.noreply.github.com>
2022-10-13 15:46:35 +0800

    seven commit for pull test

--- hello-app/src/src/com/itheima/variable/VariableDemo2.java
+++ hello-app/src/src/com/itheima/variable/VariableDemo2.java
@@ -15,6 +15,7 @@ public class VariableDemo2 {
           System.out.println(age+'3');
           System.out.println(age+'4');
           System.out.println("push test");
+          System.out.println("pull test");
 
 //        2、变量声明后，不能存储其他类型的数据。
           // age = 35.9;
