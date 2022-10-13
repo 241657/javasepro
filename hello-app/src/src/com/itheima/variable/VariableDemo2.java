e1eb646966e70628e7a6c295bae9e88839657354
cx-cx-cx <2416571038@qq.com>
2022-10-13 15:04:32 +0800

    five commit for master branch

--- hello-app/src/src/com/itheima/variable/VariableDemo2.java
+++ hello-app/src/src/com/itheima/variable/VariableDemo2.java
@@ -9,6 +9,7 @@ public class VariableDemo2 {
           System.out.println(age);
           System.out.println(age);
           System.out.println(age);
+          System.out.println("master test!");
 
 //        2、变量声明后，不能存储其他类型的数据。
           // age = 35.9;
